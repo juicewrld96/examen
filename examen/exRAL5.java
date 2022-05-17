3cef457a065a7d170764249de4032c196f155106
juicewrdl96 <ronylucana2001@gmail.com>
2022-05-17 23:36:33 +0000

    jahsashj

--- examen/exRAL5.java
+++ examen/exRAL5.java
@@ -2,7 +2,31 @@ package examen;
 
 public class exRAL5 {
     public static void main(String[] args) {
-        
+    import java.util.Scanner;
+
+/**
+ * pregunta5
+ */
+public class pregunta5 {
+    static Scanner lt= new Scanner(System.in);
+
+    public static void main(String[] args) {
+    
+        System.out.println("Ingrese la pregunta");
+        int pregunta = lt.nextInt();
+        switch (pregunta) 
+        {
+          case 1:
+          pregunta1examenFRFM(RALL) ; break;
+          case 2:
+          pregunta2examenFRFM(RALL) ; break;
+          case 3:
+          pregunta3examenFRFM(RALL); break;
+          case 4:
+          pregunta4examenFRFM(RALL); break;
+            
+            default: break;
+        }    
     }
     }
 }
